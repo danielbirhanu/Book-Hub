cf078c6481d21feae72a6b938a4104d63b159eb1
danielbirhanu <danielbir49@gmail.com>
2025-05-05 17:49:45 +0300

    Add update user route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile } from '../controllers/userController.js'
+import { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile } from '../controllers/userController.js'
 import {authenticate, authorizeAdmin} from "../middlewares/authMiddleware.js"
 
 const router = express.Router()
@@ -7,6 +7,6 @@ const router = express.Router()
 router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers)
 router.route('/auth').post(loginUser)
 router.route('/logout').post(logoutCurrentUser)
-router.route('/profile').get(authenticate, getCurrentUserProfile)
+router.route('/profile').get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile)
 
 export default router
