e88debc6791e938c7a3fd4a4b7311e3038922096
danielbirhanu <danielbir49@gmail.com>
2025-05-10 15:04:45 +0300

    Create loader component

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,7 +1,6 @@
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
-}
-
-export default Loader
+    <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-teal-500 border-opacity-50"></div>
+  );
+};
+export default Loader;
