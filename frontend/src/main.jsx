fb6846f179aa13e8f66e6b7c5132fa7f323fea2d
danielbirhanu <danielbir49@gmail.com>
2025-05-10 14:45:11 +0300

    Create register page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,11 +8,15 @@ import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
+import Login from "./pages/Auth/Login.jsx";
+import Register from "./pages/Auth/Register.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Route>
   )
 );
