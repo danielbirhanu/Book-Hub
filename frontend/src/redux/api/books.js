b862a45450ab1d5053880eadc803de1dc83a4c00
danielbirhanu <danielbir49@gmail.com>
2025-06-17 08:41:43 +0300

    Create rtk query endpoints for books routes

---
+++ frontend/src/redux/api/books.js
@@ -0,0 +1,88 @@
+
+import { apiSlice } from "./apiSlice";
+import { BOOK_URL, UPLOAD_URL } from "../constants";
+
+export const booksApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllBooks: builder.query({
+      query: () => `${BOOK_URL}/all-books`,
+    }),
+    createBook: builder.mutation({
+      query: (newBook) => ({
+        url: `${BOOK_URL}/create-book`,
+        method: "POST",
+        body: newBook,
+      }),
+    }),
+
+    updateBook: builder.mutation({
+      query: ({ id, updatedBook }) => ({
+        url: `${BOOK_URL}/update-book/${id}`,
+        method: "PUT",
+        body: updatedBook,
+      }),
+    }),
+
+    addBookReview: builder.mutation({
+      query: ({ id, rating, comment }) => ({
+        url: `${BOOK_URL}/${id}/reviews`,
+        method: "POST",
+        body: { rating, id, comment },
+      }),
+    }),
+
+    deleteComment: builder.mutation({
+      query: ({ bookId, reviewId }) => ({
+        url: `${BOOK_URL}/delete-comment`,
+        method: "DELETE",
+        body: { bookId, reviewId },
+      }),
+    }),
+
+    deleteBook: builder.mutation({
+      query: (id) => ({
+        url: `${BOOK_URL}/delete-book/${id}`,
+        method: "DELETE",
+      }),
+    }),
+
+    getSpecificBook: builder.query({
+      query: (id) => `${BOOK_URL}/specific-book/${id}`,
+    }),
+
+    uploadImage: builder.mutation({
+      query: (formData) => ({
+        url: `${UPLOAD_URL}`,
+        method: "POST",
+        body: formData,
+      }),
+    }),
+
+    getNewBooks: builder.query({
+      query: () => `${BOOK_URL}/new-books`,
+    }),
+
+    getTopBooks: builder.query({
+      query: () => `${BOOK_URL}/top-books`,
+    }),
+
+    getRandomBooks: builder.query({
+      query: () => `${BOOK_URL}/random-books`,
+    }),
+  }),
+});
+
+export const {
+  useGetAllBooksQuery,
+  useCreateBookMutation,
+  useUpdateBookMutation,
+  useAddBookReviewMutation,
+  useDeleteCommentMutation,
+  useGetSpecificBookQuery,
+  useUploadImageMutation,
+  useDeleteBookMutation,
+  //
+  useGetNewBooksQuery,
+  useGetTopBooksQuery,
+  useGetRandomBooksQuery,
+} = booksApiSlice;
