c4a77ae0d5424f3142d8bb533cd30ca026c77487
danielbirhanu <danielbir49@gmail.com>
2025-08-09 18:25:17 +0300

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Login from "./pages/Auth/Login.jsx";
 import Navigation from "./pages/Auth/Navigation.jsx";
 
 const App = () => {
